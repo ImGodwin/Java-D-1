aa42b8fe7d5d6f4a1859f0b830c89276a0b7ff1c
Godwin <godwinmail6@gmail.com>
2023-10-02 15:24:59 +0200

    imported java.util.scanner

--- Calculator.java
+++ Calculator.java
@@ -1,7 +1,34 @@
+import java.util.Scanner;
+
+
 public class Calculator
 {
     public static void main(String[] args)
     {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Add the length of the rectangle");
+        int rectangleLength = Integer.parseInt(input.nextLine());
+
+        System.out.println("Add the length width");
+        int rectangleWidth = Integer.parseInt(input.nextLine());
+
+        System.out.println("Confirm even number");
+        int confirmEvenNum = Integer.parseInt(input.nextLine());
+
+        System.out.println("Add num");
+        int triangleSideA = Integer.parseInt(input.nextLine());
+
+        System.out.println("Add num");
+        int triangleSideB = Integer.parseInt(input.nextLine());
+
+        System.out.println("Add num");
+        int triangleSideC = Integer.parseInt(input.nextLine());
+
+        //System.out.println(rectangleLength, rectangleWidth, confirmEvenNum, triangleSideA, triangleSideB, triangleSideC);
+
 
     }
+
+
 }
