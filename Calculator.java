4812f68a018b3fdff1fe51765363adff288ef81d
Godwin <godwinmail6@gmail.com>
2023-10-02 16:22:03 +0200

    all inputs calculated. Even number, Triangle area and Rectangle

--- Calculator.java
+++ Calculator.java
@@ -10,25 +10,62 @@ public class Calculator
         System.out.println("Add the length of the rectangle");
         int rectangleLength = Integer.parseInt(input.nextLine());
 
-        System.out.println("Add the length width");
+        System.out.println("Add the width width of the rectangle");
         int rectangleWidth = Integer.parseInt(input.nextLine());
+
+        System.out.println("Rectangle length and width added: " + addingRectangleSides(rectangleLength, rectangleWidth));
+
 
         System.out.println("Confirm even number");
         int confirmEvenNum = Integer.parseInt(input.nextLine());
 
-        System.out.println("Add num");
+        System.out.println(confirmEvenNumMethod(confirmEvenNum));
+
+
+        System.out.println("Add num Side A");
         int triangleSideA = Integer.parseInt(input.nextLine());
 
-        System.out.println("Add num");
+        System.out.println("Add num Side B");
         int triangleSideB = Integer.parseInt(input.nextLine());
 
-        System.out.println("Add num");
+        System.out.println("Add num Side C");
         int triangleSideC = Integer.parseInt(input.nextLine());
 
-        //System.out.println(rectangleLength, rectangleWidth, confirmEvenNum, triangleSideA, triangleSideB, triangleSideC);
+        System.out.println("Area of triangle: " + areaOfTriangle(triangleSideA, triangleSideB, triangleSideC));
+
+
+
 
 
     }
 
+    public static int addingRectangleSides(int s1, int s2)
+    {
+        return ((s1 + s2) * 2);
+    }
 
+    public static int confirmEvenNumMethod(int num1)
+    {
+        if (num1 % 2 == 0)
+        {
+            return 0;
+        } else {
+            return 1;
+        }
+    }
+
+    public static int areaOfTriangle(double a, double b, double c)
+    {
+    if ((a + b) > c && (a + c) > b && (b + c) > a)
+    {
+        //first take half of the angle
+        int allAngles = (int)(a + b + c) / 2;
+
+        int area;
+        area = (int)Math.sqrt(allAngles * (allAngles - a) * (allAngles - b) * (allAngles - c));
+        return area;
+    }else {
+        return (int)2.5;
+    }
+    }
 }
