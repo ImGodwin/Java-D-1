c7f9eeb0c2444808d8dbc5c6fac62e9ae96591a5
Godwin <godwinmail6@gmail.com>
2023-10-02 14:21:26 +0200

    Scanner created with the possibility of receiving inputs from the user

--- User.java
+++ User.java
@@ -1,5 +1,20 @@
-public class User {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class User {
+    public static void main(String[] args)
+    {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Name your favorite city in Italy?");
+        String name = input.nextLine();
+
+        System.out.println("Where is this city located?");
+        String location = input.nextLine();
+
+        System.out.println("What do you love about this city?");
+        String interest = input.nextLine();
     }
+
+
 }
